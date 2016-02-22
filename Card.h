ca61d998db9ba26d63fa15ebf89957ddadb59b78
oscko391 <oscko391@student.liu.se>
2016-02-22 15:49:51 +0100

    card.h and card.cpp ver.1.0, CARGLASS

--- Card.h
+++ Card.h
@@ -1,16 +1,18 @@
-# include <glm/glm.hpp>
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
 
 class Card
 {
 private:
     int height = 200;
     int width= 120;
-    double lifeTime = 30;
-    std::vector<string> *categories;
-    string svHeader;
-    string svText;
-    string enHeader;
-    string enText;
+    double lifeTime = 30.0;
+    std::vector<std::string> categories;
+    std::string svHeader;
+    std::string svText;
+    std::string enHeader;
+    std::string enText;
     bool isSwede = true; // limiting us to 2 languages
     glm::vec3 pos;
     glm::vec2 velocity;
@@ -18,7 +20,7 @@ private:
 public:
     //constructors
     Card(); //default construtor
-    Card(std::vector<string> inCat, string inSvH, string inSvT, string inEnH, string inEnT, bool lang, glm::vec3 inPos, glm::vec2 inVel);
+    Card(std::vector<std::string> inCat, std::string inSvH, std::string inSvT, std::string inEnH, std::string inEnT, bool lang, glm::vec3 inPos, glm::vec2 inVel);
 
     //Transformation functions
     void move(glm::vec2 velocity);
@@ -27,11 +29,11 @@ public:
     int getHeight();
     int getWidth();
     double getLifeTime();
-    std::vector<string> getCategories();
-    string getSvHeader();
-    string getSvText();
-    string getEnHeader();
-    string getEnText();
+    std::vector<std::string> getCategories();
+    std::string getSvHeader();
+    std::string getSvText();
+    std::string getEnHeader();
+    std::string getEnText();
     bool getIsSwede();
     glm::vec3 getPos();
     glm::vec2 getVelocity();
@@ -44,6 +46,7 @@ public:
     void setPos();
     void setVelocity();
 
-    //if eng ->set lang to swe
     void changeLang();
 };
+
+    //if eng ->set lang to swe
