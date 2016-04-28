cd5d57c89cfbb092a35cd3e2b595864ef14e4c19
myrydberg <my.rydberg@telia.com>
2016-04-28 11:21:11 +0200

    Updated rendering and loading of textures
    move turns the complete card, no shadow

--- PhotoCard.cpp
+++ PhotoCard.cpp
@@ -47,34 +47,15 @@ void PhotoCard::render( SDL_Renderer* gRenderer) // Blir error atm
     //Set rendering space and render to screen
     
     SDL_Rect renderQuad = {static_cast<int>(getPos().x) , static_cast<int>(getPos().y), getWidth(), getHeight() };
-    SDL_Rect whiteQuad = {static_cast<int>(getPos().x) - 5, static_cast<int>(getPos().y) - 5, getWidth() + 10, getHeight() + 10 };
-    SDL_Rect blackQuad = {static_cast<int>(getPos().x) - 10, static_cast<int>(getPos().y) - 10, getWidth() + 20, getHeight() + 20 };
-    SDL_Rect headQuad = {static_cast<int>(getPos().x) + getWidth()/60 , static_cast<int>(getPos().y) + getWidth()/60  , getWidth() - getWidth()/30  , getWidth() / 10};
+        
+    SDL_RenderCopyEx( gRenderer, theTextures[texIndex], NULL, &renderQuad, getAngle(), NULL, SDL_FLIP_NONE );
 
-    if (getReading()) {
-        headQuad = {static_cast<int>(getPos().x) + getWidth()/60 , static_cast<int>(getPos().y) + getWidth()/60 , getWidth() - getWidth()/30 , getHeight() - getWidth()/30};
-    }
-    
-    //SDL_Rect headText =
-    
-    SDL_SetRenderDrawColor( gRenderer, 0x10, 0x20, 0x50, 0x35 );
-    SDL_RenderFillRect( gRenderer, &blackQuad );
-    
-    SDL_SetRenderDrawColor( gRenderer, 0xEE, 0xEE, 0xEE, 0xFF );
-    SDL_RenderFillRect( gRenderer, &whiteQuad );
-    
-    SDL_RenderCopy( gRenderer, theTextures[texIndex], NULL, &renderQuad );
-    
-    SDL_SetRenderDrawColor( gRenderer, 0xDD, 0xDD, 0xDD, 0xCC );
-    SDL_RenderFillRect( gRenderer, &headQuad );
-    
-    SDL_RenderCopyEx(gRenderer, getHeader(), NULL, &headQuad, 0, NULL, SDL_FLIP_NONE);
 }
 
 SDL_Texture* PhotoCard::loadingTex(SDL_Renderer* r, std::string path)
 {
     //The final texture
-    SDL_Texture* newTexture = NULL;
+    SDL_Texture* imgTexture = NULL;
     
     //Load image at specified path
     SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
@@ -85,8 +66,8 @@ SDL_Texture* PhotoCard::loadingTex(SDL_Renderer* r, std::string path)
     else
     {
         //Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface( r, loadedSurface );
-        if( newTexture == NULL )
+        imgTexture = SDL_CreateTextureFromSurface( r, loadedSurface );
+        if( imgTexture == NULL )
         {
             printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
         }
@@ -94,6 +75,24 @@ SDL_Texture* PhotoCard::loadingTex(SDL_Renderer* r, std::string path)
         //Get rid of old loaded surface
         SDL_FreeSurface( loadedSurface );
     }
+    int w, h;
+    SDL_QueryTexture(imgTexture, NULL, NULL, &w, &h);
+    
+    SDL_Texture* newTexture = SDL_CreateTexture( r, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, w + w/25 , h + w/25 );
+    SDL_SetRenderTarget( r, newTexture );
+    
+    SDL_SetRenderDrawColor( r, 0xEE, 0xEE, 0xEE, 0xFF );
+    SDL_RenderFillRect( r , NULL );
+    
+    SDL_Rect renderQuad = {w/50 , w/50, w, h };
+    SDL_RenderCopyEx( r, imgTexture , NULL, &renderQuad, NULL, NULL, SDL_FLIP_NONE );
+    
+    SDL_Rect headQuad = {w/25, w/25  , w - w/25  , w / 10};
+    SDL_SetRenderDrawColor( r, 0xDD, 0xDD, 0xDD, 0xCC );
+    SDL_RenderFillRect( r, &headQuad );
+    SDL_RenderCopyEx(r, getHeader(), NULL, &headQuad, 0, NULL, SDL_FLIP_NONE);
+    
+    SDL_SetRenderTarget( r, NULL );
     
     return newTexture;
 }
